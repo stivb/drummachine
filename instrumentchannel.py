d080477e2333a1d1d67254d97a61ecdbf17410a7
stivb <stivbennett@gmail.com>
2019-10-03 10:27:31 +0100

    now making the track panel dialog easier to set the voice

--- instrumentchannel.py
+++ instrumentchannel.py
@@ -36,6 +36,7 @@ class InstrumentChannel:
         self.clmct=0
 
 
+
     def clm(self):
         self.clmct+=1
         return self.clmct
@@ -50,70 +51,83 @@ class InstrumentChannel:
         self.currRow = rowNum
 
 
+        note = ttk.Notebook(self.top)
 
-        Label(self.top, text="Settings").grid(row=0, column=self.clm())
 
-        Label(self.top, text="Inst/Note").grid(row=0,column=self.clm())
-        self.txtInstrument = Entry(self.top)
+
+        noteFrame = ttk.Frame(note)
+
+        note.add(noteFrame, text="Track Operations")
+        note.pack(expand=1, fill="both")
+        #note.pack(expand=1, fill="both")
+        #tabControls.append(noteFrame)
+
+        monty = ttk.LabelFrame(noteFrame, text="Track Operations")
+        monty.grid(column=0, row=0, padx=8, pady=4)
+
+        Label(monty, text="Settings").grid(row=0, column=self.clm())
+
+        Label(monty, text="Inst/Note").grid(row=0,column=self.clm())
+        self.txtInstrument = Entry(monty)
         self.txtInstrument.insert(END, str(self.inst_or_note))
         self.txtInstrument.grid(row=0, column=self.clm())
 
-        Label(self.top, text="Channel").grid(row=0,column=self.clm())
-        self.txtChannel = Entry(self.top)
+        Label(monty, text="Channel").grid(row=0,column=self.clm())
+        self.txtChannel = Entry(monty)
         self.txtChannel.insert(END,str(self.channel))
         self.txtChannel.grid(row=0, column=self.clm())
 
-        Label(self.top, text="Velocity").grid(row=0,column=self.clm())
-        self.txtVelocity = Entry(self.top)
+        Label(monty, text="Velocity").grid(row=0,column=self.clm())
+        self.txtVelocity = Entry(monty)
         self.txtVelocity.insert(END,str(self.velocity))
         self.txtVelocity.grid(row=0, column=self.clm())
 
-        muteButton = Button(self.top, text="Mute", command=self.btnMuteUnmute)
+        muteButton = Button(monty, text="Mute", command=self.btnMuteUnmute)
         muteButton.grid(row=0, column=self.clm())
 
         self.clm0()
 
-        #s = Separator(self.top, orient=VERTICAL).grid(column=0, row=0, rowspan=10, sticky='ns')
+        #s = Separator(monty, orient=VERTICAL).grid(column=0, row=0, rowspan=10, sticky='ns')
 
-        ttk.Separator(self.top, orient='horizontal').grid(column=0, row=1, rowspan=10, sticky='ew')
+        ttk.Separator(monty, orient='horizontal').grid(column=0, row=1, rowspan=10, sticky='ew')
 
 
         self.clm()
 
-        Label(self.top, text="Track").grid(row=2, column=self.clm())
+        Label(monty, text="Track").grid(row=2, column=self.clm())
 
-        copyButton = Button(self.top, text="Copy", command=self.btnCopyTrack)
+        copyButton = Button(monty, text="Copy", command=self.btnCopyTrack)
         copyButton.grid(row=2, column=self.clm())
 
-        pasteButton = Button(self.top, text="Paste", command=self.btnPasteTrack)
+        pasteButton = Button(monty, text="Paste", command=self.btnPasteTrack)
         pasteButton.grid(row=2, column=self.clm())
 
-        duplicateButton = Button(self.top, text="Duplicate", command=self.btnDuplicateTrackSection)
+        duplicateButton = Button(monty, text="Duplicate", command=self.btnDuplicateTrackSection)
         duplicateButton.grid(row=2, column=self.clm())
 
-        transposeButton = Button(self.top, text="Transpose", command=self.doPermanentTransposition)
+        transposeButton = Button(monty, text="Transpose", command=self.doPermanentTransposition)
         transposeButton.grid(row=2, column=self.clm())
 
         self.transposeOffset = StringVar()
         self.transposeOffset.set('0')  # set the default option
-        transposeOffsetDropDown = OptionMenu(self.top, self.transposeOffset, 0,1,2,3,4,5,6,7,8,9,10,11,12)
+        transposeOffsetDropDown = OptionMenu(monty, self.transposeOffset, 0,1,2,3,4,5,6,7,8,9,10,11,12)
         transposeOffsetDropDown.grid(row=2, column=self.clm())
 
-        deleteButton = Button(self.top, text="Delete Notes", command=self.delNotes)
+        deleteButton = Button(monty, text="Delete Notes", command=self.delNotes)
         deleteButton.grid(row=2, column=self.clm())
 
-        Label(self.top, text="From").grid(row=2, column=self.clm())
+        Label(monty, text="From").grid(row=2, column=self.clm())
 
         self.defaultFrom = StringVar()
         self.defaultFrom.set('1')  # set the default option
-        fromDropDown= OptionMenu(self.top, self.defaultFrom,1, 4, 9,13,17, 21,25,29)
+        fromDropDown= OptionMenu(monty, self.defaultFrom,1, 4, 9,13,17, 21,25,29)
         fromDropDown.grid(row=2, column=self.clm())
 
-        Label(self.top, text="To").grid(row=2, column=self.clm())
+        Label(monty, text="To").grid(row=2, column=self.clm())
 
         self.defaultTo = StringVar()
         self.defaultTo.set('32')  # set the default option
-        toDropDown = OptionMenu(self.top, self.defaultTo, 4, 8, 12, 16, 20, 24, 28,32)
+        toDropDown = OptionMenu(monty, self.defaultTo, 4, 8, 12, 16, 20, 24, 28,32)
         toDropDown.grid(row=2, column=self.clm())
 
         self.clm0()
@@ -121,19 +135,33 @@ class InstrumentChannel:
 
 
 
-        b = Button(self.top, text="OK", command=self.ok)
+        b = Button(monty, text="OK", command=self.ok)
         b.grid(row=3, column=self.clm())
 
-        c = Button(self.top, text="Cancel", command=self.cancel)
+        c = Button(monty, text="Cancel", command=self.cancel)
         c.grid(row=3, column=self.clm())
 
-        r= Button(self.top, text="Record", command=self.recordPopup)
+        r= Button(monty, text="Record", command=self.recordPopup)
         r.grid(row=4, column=self.clm())
+
+        noteFrame2 = ttk.Frame(note)
+
+        note.add(noteFrame2, text="Voice Chooser")
+        note.pack(expand=1, fill="both")
+
+        monty2 = ttk.LabelFrame(noteFrame2, text="Choose Voice")
+        monty2.grid(column=0, row=0, padx=8, pady=4)
+
+
+        for i in range(4):
+            for j in range(32):
+                btn = Button(monty2, text=str(i*32+j+1), width=3)
+                btn.grid(column=j, row=i)
 
 
 
     def recordPopup(self):
-        self.top.destroy()
+        noteFrame.destroy()
         piano.Piano(self.parent, None,self.currRow)
         self.parent.play_in_thread_looped()
 
@@ -142,10 +170,10 @@ class InstrumentChannel:
         self.inst_or_note = int(self.txtInstrument.get())
         self.channel = int(self.txtChannel.get())
         self.velocity = int(self.txtVelocity.get())
-        self.top.destroy()
+        noteFrame.destroy()
 
     def cancel(self):
-        self.top.destroy()
+        noteFrame.destroy()
 
     def delNotes(self):
 
--- tabbedpanel.py
+++ tabbedpanel.py
@@ -16,20 +16,24 @@ class OOP():
 
     def createWidgets(self):
 
-        tabControls = []
+        #creates "notebook"
         note = ttk.Notebook(self.win)
 
         for i in range(0, 8):
             mystr = str(i*32) + '-' + str((i*32)+32)
 
+            #creates a frame from the notebook
             thistab = ttk.Frame(note)
-            tabControls.append(thistab)
+            #adds teh frame to the notebook
             note.add(thistab, text=mystr)
+            # packs it
             note.pack(expand=1, fill="both")
+            # creates a "labelframe" inside the frame inside the notebook
             self.monty = ttk.LabelFrame(thistab, text=mystr + "x")
+            # sets up a grid for it
             self.monty.grid(column=0, row=0, padx=8, pady=4)
-            for j in range(i*32, (i*32)+32):
-                btn = Button(self.monty, text=str(j+1),width=3)
+            for j in range(i * 32, (i * 32) + 32):
+                btn = Button(self.monty, text=str(j + 1), width=3)
                 btn.grid(column=j, row=1)
 
 
